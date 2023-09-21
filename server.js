4e86b70408509c9ac403ec89edefa396538b1ad1
TamirKol <tamirk9596@gmail.com>
2023-09-21 18:43:53 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,117 @@
+import express from 'express'
+import { toyService } from './services/toy.service.js'
+import cors from 'cors'
+import { loggerService } from './services/logger.service.js';
+import cookieParser from 'cookie-parser'
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const app = express()
+
+// App Configuration
+const corsOptions = {
+    origin: [
+        'http://127.0.0.1:8080',
+        'http://localhost:8080',
+        'http://127.0.0.1:5173',
+        'http://localhost:5173',
+    ],
+    credentials: true
+}
+
+app.use(cors(corsOptions))
+app.use(cookieParser()) // for res.cookies
+app.use(express.json()) // for req.body
+app.use(express.static('public'))
+
+
+//List
+app.get('/api/toy', (req, res) => {
+    // const {filterBy = {}, sort = {}} = req.query.params
+    // console.log("req.query.params:", req.query.params)
+    
+    toyService.query()
+        .then(toys => {
+            res.send(toys)
+        })
+        .catch(err => {
+            console.log('Had issues getting toys', err);
+            res.status(400).send({ msg: 'Had issues getting toys' })
+        })
+})
+
+// Read - getById
+app.get('/api/toy/:toyId', (req, res) => {
+    const { toyId } = req.params
+    toyService.get(toyId)
+        .then(toy => {
+            // toy.msgs =['HEllo']
+            res.send(toy)
+        })
+        .catch(err => {
+            loggerService.error('Cannot get toy', err)
+            res.status(400).send(err)
+        })
+})
+
+// Remove
+app.delete('/api/toy/:toyId', (req, res) => {
+    const { toyId } = req.params
+    toyService.remove(toyId)
+        .then(msg => {
+            res.send({ msg, toyId })
+        })
+        .catch(err => {
+            loggerService.error('Cannot delete toy', err)
+            res.status(400).send('Cannot delete toy, ' + err)
+        })
+})
+
+// Add
+app.post('/api/toy', (req, res) => {
+    const { name, inStock, price,labels } = req.body
+
+    const toy = {
+        name,
+        inStock,
+        labels,
+        price: +price
+    }
+    toyService.save(toy)
+        .then(savedCar => {
+            res.send(savedCar)
+        })
+        .catch(err => {
+            loggerService.error('Cannot add toy', err)
+            res.status(400).send('Cannot add toy')
+        })
+})
+
+// Edit
+app.put('/api/toy', (req, res) => {
+    const { name, inStock, price,labels, _id } = req.body
+    const toy = {
+        _id,
+        name,
+        inStock,
+        labels,
+        price: +price
+    }
+    toyService.save(toy, loggedinUser)
+        .then((savedCar) => {
+            res.send(savedCar)
+        })
+        .catch(err => {
+            loggerService.error('Cannot update toy', err)
+            res.status(400).send('Cannot update toy')
+        })
+
+})
+
+app.get('/**', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
+
+const PORT = 3030
+app.listen(PORT, () => console.log(`Server ready at port ${PORT}! http://localhost:${PORT}`))
---
+++ services/toy.service.js
@@ -0,0 +1,84 @@
+import fs from 'fs'
+import { utilService } from '../services/util.service.js'
+
+const gToys = utilService.readJsonFile('data/toys.json')
+
+export const toyService = {
+    query,
+    get,
+    remove,
+    save
+}
+
+function query(filterBy) {
+
+    if (!filterBy) return Promise.resolve(gToys)
+
+    let toyToDisplay = gToys
+
+    if (filterBy.txt) {
+        const regExp = new RegExp(filterBy.txt, 'i')
+        toyToDisplay = toyToDisplay.filter(toy => regExp.test(toy.name))
+    }
+    if (filterBy.inStock) {
+        if (filterBy.inStock === "false") {
+            toyToDisplay = toyToDisplay.filter(toy => toy.inStock === 'false')
+        } else {
+            toyToDisplay = toyToDisplay.filter(toy => toy.inStock === 'true')
+        }
+    }
+    if (filterBy.labels && filterBy.labels.length > 0) {
+        toyToDisplay = toyToDisplay.filter(toy => {
+            return toy.labels.some(label => filterBy.labels.includes(label))
+        })
+    }
+
+    return toyToDisplay
+
+}
+
+function get(toyId) {
+    const toy = gToys.find(toy => toy._id === toyId)
+    if (!toy) return Promise.reject('Toy not found!')
+    return Promise.resolve(toy)
+}
+
+
+function remove(toyId) {
+    const idx = gToys.findIndex(toy => toy._id === toyId)
+    if (idx === -1) return Promise.reject('No Such Toy')
+    const toy = gToys[idx]
+    gToys.splice(idx, 1)
+    return _saveToysToFile()
+
+}
+
+function save(toy) {
+    if (toy._id) {
+        const toyToUpdate = gToys.find(currToy => currToy._id === toy._id)
+        toyToUpdate.name = toy.name
+        toyToUpdate.inStock = toy.inStock
+        toyToUpdate.price = toy.price
+        toyToUpdate.labels = toy.labels
+    } else {
+        toy._id = utilService._makeId()
+        gToys.push(toy)
+    }
+
+    return _saveToysToFile().then(() => toy)
+    // return Promise.resolve(toy)
+}
+
+function _saveToysToFile() {
+    return new Promise((resolve, reject) => {
+
+        const toysStr = JSON.stringify(gToys, null, 4)
+        fs.writeFile('data/toys.json', toysStr, (err) => {
+            if (err) {
+                return console.log(err);
+            }
+            console.log('The file was saved!');
+            resolve()
+        });
+    })
+}
